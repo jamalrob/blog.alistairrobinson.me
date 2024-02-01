1c1a5e72603ff6ac1cc0a27b7a7e77d55cdf0efb
Alistair Robinson <alistair.robinson@gmail.com>
2024-02-01 12:17:45 +0300

    Added 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,42 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import styles from '@/styles/layout.module.css';
+import postStyles from '@/styles/post.module.css';
+import Layout from '@/components/layout';
+import Head from 'next/head';
+import { settings } from '@/settings';
+import { Source_Serif_4 } from 'next/font/google'
+import { redirect } from 'next/navigation'
+
+const gfont = Source_Serif_4({
+    weight: ['400', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    display: 'swap'
+})
 
 export default function Custom404() {
-  let router = useRouter();
-  useEffect(() => {
-      if(router.asPath == '/adventure-of-hans-pfaall-edgar-allan-poe') {
-        router.push('/unparalleled-adventure-of-hans-pfaall-edgar-allan-poe')
-      }
-  })
-}
+
+    const imageStyle = {
+        width: 'auto',
+        height: 'auto',
+        maxWidth: '70%'
+    }
+
+    return (
+      <Layout>
+        <Head>
+          <title>404 Page Not Found</title>
+        </Head>
+        <article className={[postStyles.postArticle, styles.innerContainer].join(' ')}>
+          <h1 className={styles.heading2Xl}>Page not found</h1>
+            <div className={gfont.className}>
+            <p>
+                <a href="/">Go to the home page</a>
+            </p>
+            </div>
+        </article>
+      </Layout>
+    );
+
+  }
