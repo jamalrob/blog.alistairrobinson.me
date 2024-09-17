37fb35b76627ce97782e427a00690375aa8be932
Alistair Robinson <alistair.robinson@gmail.com>
2024-09-17 10:17:27 +0100

    About edit

--- pages/about.js
+++ pages/about.js
@@ -29,7 +29,7 @@ export default function About() {
           <h1 className={styles.heading2Xl}>About</h1>
             <div className={gfont.className}>
             <p>
-                On this website I write about things, particularly books. I&#8217;m into science fiction, speculative fiction in general, literary fiction, and philosophy.
+                On this website I write about things, particularly books. I&#8217;m into science fiction, speculative fiction in general, literary fiction, and philosophy. Sometimes I put my own short fiction here too.
             </p>
             <p>
                 Otherwise, I manage one of the biggest and best philosophy discussion forums on the internet at <a href="https://thephilosophyforum.com" target="_blank">thephilosophyforum.com</a>, I&#8217;m sometimes a software developer, and I&#8217;ve been known to dabble with saxophone and camera.
