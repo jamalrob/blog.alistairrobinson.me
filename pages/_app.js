9e79d1636344832635c97d41d7c9fdfc8540f938
Alistair Robinson <alistair.robinson@gmail.com>
2025-12-11 19:13:36 +0300

    Change code block style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import { Barlow_Semi_Condensed as MySansSerifFont } from 'next/font/google'
 import { Literata as MySerifFont } from 'next/font/google'
+import { Google_Sans_Code as MyMonoFont } from 'next/font/google'
+
 
 const sans = MySansSerifFont({
   weight: ['400', '600'],
@@ -12,6 +14,13 @@ const sans = MySansSerifFont({
 const serif = MySerifFont({
   weight: ['400', '600'],
   style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const mono = MyMonoFont({
+  weight: ['400', '700'],
+  style: ['normal'],
   subsets: ['latin'],
   display: 'swap',
 })
